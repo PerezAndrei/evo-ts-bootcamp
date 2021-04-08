4197875ecb656b9041145c0b576cc9c57ae320f7
PerezAndrei <andrei_1089@mail.ru>
2021-04-09 00:30:48 +0300

    feat: layout, empty components

---
+++ homeworks/01-bubble-sort/src/components/BubbleSort.tsx
@@ -0,0 +1,13 @@
+import { Header } from './Header'
+
+export function BubleSort() {
+    return (
+        <div className="bs-layout">
+            <header>
+                <Header></Header>
+            </header>
+            <main></main>
+            <footer></footer>
+        </div>
+    );
+}
